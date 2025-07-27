e89fc8790db3eebf5d038fc11f54cfa6d14a06ca
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-27 14:18:57 +0100

    replace for loop with map

--- prep/script.js
+++ prep/script.js
@@ -1,19 +1,20 @@
-const films = [{
-  title: "Killing of Flower Moon",
-  director: "Martin Scorsese",
-  times: ["15:35"],
-  certificate: "15",
-  duration: 112,
-}, {
-  title: "Typist Artist Pirate King",
-  director: "Carol Morley",
-  times: ["15:00", "20:00"],
-  certificate: "12A",
-  duration: 108,
-}];
-// Goal: How to use film card component with an array of films
-
-// How can we reuse functionality?
+const films = [
+  {
+    title: "Killing of Flower Moon",
+    director: "Martin Scorsese",
+    times: ["15:35"],
+    certificate: "15",
+    duration: 112,
+  },
+  {
+    title: "Typist Artist Pirate King",
+    director: "Carol Morley",
+    times: ["15:00", "20:00"],
+    certificate: "12A",
+    duration: 108,
+  },
+];
+// Goal: Build an array of film card and then append them to the DOM
 
 function createFilmCard(film) {
   const filmCard = document
@@ -26,9 +27,6 @@ function createFilmCard(film) {
   return filmCard;
 }
 
-// How we go through the array of films and create a card for each element
+const filmCards = films.map(createFilmCard);
 
-for (const film of films) {
-  const card = createFilmCard(film);
-  document.body.appendChild(card)
-}
+document.body.append(...filmCards);
